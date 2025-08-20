79ca82598e5f4c785a72d34790ae5ae71e6a408f
Ven <ven.wu@baifu-tech.net>
2025-08-20 11:35:10 +0800

    [Feature] Command 支援 Docker image 運行

--- Sources/APKSignKey/Command.swift
+++ Sources/APKSignKey/Command.swift
@@ -3,6 +3,9 @@ import Foundation
 struct Command {
     @discardableResult
     static func run(_ command: String, arguments: [String]) throws -> String {
+        let semaphore = DispatchSemaphore(value: 0)
+        var result: Result<String, Error>?
+        
         let process = Process()
         process.executableURL = URL(fileURLWithPath: "/usr/bin/env")
         process.arguments = [command] + arguments
@@ -12,20 +15,56 @@ struct Command {
         process.standardOutput = outputPipe
         process.standardError = errorPipe
         
-        try process.run()
-        process.waitUntilExit()
-        
-        let outputData = outputPipe.fileHandleForReading.readDataToEndOfFile()
-        let errorData = errorPipe.fileHandleForReading.readDataToEndOfFile()
-        
-        let output = String(data: outputData, encoding: .utf8) ?? ""
-        let error = String(data: errorData, encoding: .utf8) ?? ""
-        let combinedOutput = output + error
-        
-        if process.terminationStatus != 0 {
-            throw NSError(domain: "\(command) \(arguments.joined(separator: " "))\n\n\(output)\n\n\(error)", code: Int(process.terminationStatus))
+        // 設定完成回調(Docker container 裡面運行的時候，一定要用 terminatinHandler 才不會卡住)
+        process.terminationHandler = { process in
+            let outputData = outputPipe.fileHandleForReading.readDataToEndOfFile()
+            let errorData = errorPipe.fileHandleForReading.readDataToEndOfFile()
+            
+            let output = String(data: outputData, encoding: .utf8) ?? ""
+            let error = String(data: errorData, encoding: .utf8) ?? ""
+            let combinedOutput = output + error
+            
+            if process.terminationStatus == 0 {
+                result = .success(combinedOutput)
+            }
+            else {
+                let error = NSError(
+                    domain: "Command Error \(command)",
+                    code: Int(process.terminationStatus),
+                    userInfo: [NSLocalizedDescriptionKey: "\(arguments.joined(separator: " "))\n\n\(output)\n\n\(error)"]
+                )
+                result = .failure(error)
+            }
+            semaphore.signal()
         }
         
-        return combinedOutput
+        do {
+            try process.run()
+            let timeoutResult = semaphore.wait(timeout: .now() + 5)
+            if timeoutResult == .timedOut {
+                process.terminate()
+                throw NSError(
+                    domain: "Command Timeout Error",
+                    code: -1,
+                    userInfo: [NSLocalizedDescriptionKey: "\(command) operation timed out"]
+                )
+            }
+        } catch {
+            result = .failure(error)
+        }
+        
+        guard let result else {
+            throw NSError(
+                domain: "Command Error \(command)",
+                code: -1,
+                userInfo: [NSLocalizedDescriptionKey: "\(command) operation failed, no result"]
+            )
+        }
+        switch result {
+        case .success(let output):
+            return output
+        case .failure(let error):
+            throw error
+        }
     }
 }
